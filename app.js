026fffcd9352b4fb84c724e8c9f666506893cab9
alihannatas <alihan2644@gmail.com>
2023-07-18 23:32:03 +0300

    ejs template engine added.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import express from "express";
 
-
-const app= express();
+const app = express();
 const port = 3000;
 
-app.use("/",(req,res,next)=> {
-    res.send("Hello Wordd");
+//ejs template engine
+app.set("view engine", "ejs");
+
+//static files middleware
+app.use(express.static('public'));
+
+app.get("/", (req, res, next) => {
+    res.render("index"); 
 })
 
-app.listen(port,()=> {
+app.get("/about", (req, res, next) => {
+    res.render("about");
+})
+
+app.listen(port, () => {
     console.log("Server is running")
 })
