b4766b1483f13256885e8da5bb3381c702e5b207
alihannatas <alihan2644@gmail.com>
2023-08-06 02:07:21 +0300

    Contact page has been made and related functions have been added.

--- controller/pageController.js
+++ controller/pageController.js
@@ -1,3 +1,5 @@
+import nodemailler from "nodemailer";
+
 const getIndexPage = (req, res) => {
   res.render("index", {
     link: "index",
@@ -29,4 +31,51 @@ const getLogout = (req, res) => {
   res.redirect("/");
 };
 
-export { getAboutPage, getIndexPage, getRegisterPage, getLoginPage, getLogout };
+const getContact = (req, res) => {
+  res.render("contact", {
+    link: "contact",
+  });
+};
+
+const sendMail = async (req, res) => {
+  try {
+    let transporter = nodemailler.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: process.env.SEND_MAIL,
+        pass: process.env.SEND_MAIL_PASSWORD,
+      },
+    });
+
+    // send mail with defined transport object
+    await transporter.sendMail({
+      to: 'alihan2644@gmail.com', // list of receivers
+      subject: `LensLight Contact`, // Subject line
+      html: `
+         <div style>   From:  ${req.body.email} </div>  
+         <div>Subject:  ${req.body.name} </div>
+         <div>   text:  ${req.body.message} </div>
+       `, // html body
+    });
+
+    res.status(200).json({ succeded: true });
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      error,
+    });
+  }
+};
+
+export {
+  getAboutPage,
+  getIndexPage,
+  getRegisterPage,
+  getLoginPage,
+  getLogout,
+  getContact,
+  sendMail,
+};
